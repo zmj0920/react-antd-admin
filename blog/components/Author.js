948f67e729d9213184a8b63ceef58cc1fa2e61b2
zmj0920 <mj960920@163.com>
2019-12-09 23:43:51 +0800

    update author

--- blog/components/Author.js
+++ blog/components/Author.js
@@ -1,17 +1,29 @@
-import {Avatar,Divider} from 'antd'
+import { Avatar, Divider, Tag } from 'antd'
 import '../static/style/components/author.css'
 
-const Author =()=>{
+const Author = () => {
 
     return (
         <div className="author-div comm-box">
-            <div> <Avatar size={100} src="../static/img/16cfa3238800341b.png"  /></div>
+            <div> <Avatar size={100} src="../static/img/16cfa3238800341b.png" /></div>
             <div className="author-introduction">
-                前端自由写手，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
+                <div class="author-name">君吟</div>
+                <div>   前端自由写手，WEB和移动前端开发</div>
+                <div className="author-tag">
+                    <Tag color="green">JavaScript</Tag>
+                    <Tag color="magenta">react</Tag>
+                    <Tag color="red">angular</Tag>
+                    <Tag color="volcano">vue</Tag>
+                    <Tag color="orange">nest</Tag>
+                    <Tag color="gold">golang</Tag>
+                    <Tag color="lime">.net</Tag>
+                    <Tag color="cyan">技术菜</Tag>
+                  
+                </div>
                 <Divider>社交账号</Divider>
-                <Avatar size={28} icon="github" className="account"  />
-                <Avatar size={28} icon="qq"  className="account" />
-                <Avatar size={28} icon="wechat"  className="account"  />
+                <Avatar size={28} icon="github" className="account" />
+                <Avatar size={28} icon="qq" className="account" />
+                <Avatar size={28} icon="wechat" className="account" />
 
             </div>
         </div>
