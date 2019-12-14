3e97a5f9eb72f6df78978cd133038f5a4c26c9de
zmj0920 <mj960920@163.com>
2019-12-14 14:53:43 +0800

    update

--- service/app/router/admin.js
+++ service/app/router/admin.js
@@ -5,6 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  // eslint-disable-next-line no-unused-vars
   // const adminauth = app.middleware.adminauth();
   router.get('/admin/index', controller.admin.main.index);
   router.post('/admin/checkLogin', controller.admin.main.checkLogin);
