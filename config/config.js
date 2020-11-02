5d0a98a011618aaec4f60781ef59a04f1d15befd
zhangmengjie <mj960920@163.com>
2020-11-02 17:53:46 +0800

    更新配置文件

--- config/config.js
+++ config/config.js
@@ -4,6 +4,10 @@ import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import pageRoutes from './router.config';
 const { REACT_APP_ENV } = process.env;
+const path = require("path");
+// const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const isEnvProduction = process.env.NODE_ENV === "production";
+const assetDir = "static";
 export default defineConfig({
   // hash: false,
   history: { type: 'hash' },
@@ -48,12 +52,76 @@ export default defineConfig({
     logLevel: 'info',
     defaultSizes: 'gzip', // stat  // gzip
   },
-  chunks: ['vendors', 'umi'],
+  chunks: ['react', 'utils', 'charts', 'vendors', 'umi'],
+  // 生产环境去除console日志打印
+  terserOptions: {
+    compress: {
+      drop_console: isEnvProduction,
+    },
+  },
   chainWebpack(memo, { env, webpack, createCSSRule }) {
-    // 删除 umi 内置插件
-    // memo.plugins.delete('progress');
-    // memo.plugins.delete('friendly-error');
-    // memo.plugins.delete('copy');
+
+    // 修改js，js chunk文件输出目录
+    memo.output
+      .filename(assetDir + '/js/[name].js')
+      .chunkFilename(assetDir + '/js/[name].chunk.js')
+    // 修改css输出目录
+    memo.plugin("extract-css").tap(() => [
+      {
+        filename: `${assetDir}/css/[name].css`,
+        chunkFilename: `${assetDir}/css/[name].chunk.css`,
+        ignoreOrder: true,
+      },
+    ]);
+    // 修改图片输出目录
+    memo.module
+      .rule("images")
+      .test(/\.(png|jpe?g|gif|webp|ico)(\?.*)?$/)
+      .use("url-loader")
+      .loader(require.resolve("url-loader"))
+      .tap((options) => {
+        const newOptions = {
+          ...options,
+          name: assetDir + "/img/[name].[ext]",
+          fallback: {
+            ...options.fallback,
+            options: {
+              name: assetDir + "/img/[name].[ext]",
+              esModule: false,
+            },
+          },
+        };
+        return newOptions;
+      });
+
+    // 修改svg输出目录
+    memo.module
+      .rule("svg")
+      .test(/\.(svg)(\?.*)?$/)
+      .use("file-loader")
+      .loader(require.resolve("file-loader"))
+      .tap((options) => ({
+        ...options,
+        name: assetDir + "/img/[name].[ext]",
+      }));
+
+    // 修改fonts输出目录
+    memo.module
+      .rule("fonts")
+      .test(/\.(eot|woff|woff2|ttf)(\?.*)?$/)
+      .use("file-loader")
+      .loader(require.resolve("file-loader"))
+      .tap((options) => ({
+        ...options,
+        name: assetDir + "/fonts/[name].[ext]",
+        fallback: {
+          ...options.fallback,
+          options: {
+            name: assetDir + "/fonts/[name].[ext]",
+            esModule: false,
+          },
+        },
+      }));
     memo.merge({
       optimization: {
         minimize: true,
@@ -65,31 +133,31 @@ export default defineConfig({
           cacheGroups: {
             // 组件库相关
             react: {
-              name: "react",
-              chunks: "all",
+              name: 'react',
+              chunks: 'all',
               test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom|moment|antd|@ant-design)[\\/]/,
-              priority: 12
+              priority: 12,
             },
             // 工具库相关
             utils: {
-              name: "utils",
-              chunks: "all",
+              name: 'utils',
+              chunks: 'all',
               test: /[\\/]node_modules[\\/](lodash|ramda)[\\/]/,
-              priority: 11
+              priority: 11,
             },
             // 图表库相关
             charts: {
-              name: "charts",
-              chunks: "all",
+              name: 'charts',
+              chunks: 'all',
               test: /[\\/]node_modules[\\/](echarts|bizcharts|@antv)[\\/]/,
-              priority: 11
+              priority: 11,
             },
             vendors: {
-              name: "vendors",
-              chunks: "all",
+              name: 'vendors',
+              chunks: 'all',
               test: /[\\/]node_modules[\\/]/,
-              priority: 10
-            }
+              priority: 10,
+            },
           },
         },
       }
